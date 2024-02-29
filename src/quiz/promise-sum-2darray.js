3d3eeff1d97cd0c576123ebc7e7b6fb4d0cf7a6a
LiamLin <137009133+LiamLin09@users.noreply.github.com>
2024-02-29 10:23:40 -0500

    Update promise-sum-2darray.js

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,5 +1,26 @@
+function sumRow(row) {
+    return new Promise((resolve) => {
+        let sum = 0;
+        for (const num of row) {
+            sum += num;
+        }
+        resolve(sum);
+    });
+}
+
+function sum2DArrayConcurrently(array2D) {
+    const promises = array2D.map(row => sumRow(row));
+
+    return Promise.all(promises)
+        .then((sums) => sums.reduce((total, current) => total + current, 0));
+}
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+sum2DArrayConcurrently(array2D)
+    .then(sum => console.log(`Total sum: ${sum}`))
+    .catch(err => console.error(err));
